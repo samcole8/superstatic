0f84900c7053698324015899b73ec92f4570684f
samcole8 <sam@samcole.me>
2024-10-08 22:46:37 +0100

    Add argument parser
    - Added code to manage CLI arguments
    - Added a mandatory CLI argument "web_root" to contain the web root
      directory
    - Added an optional CLI argument "template" to contain the path to the
      template

--- superstatic/superstatic.py
+++ superstatic/superstatic.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import argparse
 
 superstatic = Flask(__name__)
 
@@ -9,4 +10,18 @@ def serve(url_path=""):
     return 404
 
 
-superstatic.run()
+if __name__ == "__main__":
+    # Set up argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("web_root", type=str, help="Path to the web root directory.")
+    parser.add_argument("--template", type=str, help="Template to render (optional).")
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Define configuration variables
+    superstatic.config["TEMPLATE"] = args.template
+    superstatic.config["WEB_ROOT"] = args.web_root
+
+    # Start Flask app
+    superstatic.run()
