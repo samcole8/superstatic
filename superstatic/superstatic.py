a797cf2ef462ea661226573bdacba4709d4c8dfa
samcole8 <sam@samcole.me>
2024-10-08 23:05:45 +0100

    Add code to map URL to entrypoint
    - Added code that takes a URL path and maps it to a file on the server
      filesystem

--- superstatic/superstatic.py
+++ superstatic/superstatic.py
@@ -1,13 +1,31 @@
 from flask import Flask
 import argparse
+import os
 
 superstatic = Flask(__name__)
+
+
+EXTENSIONS = ["html", "md"]
+
+
+def map_url(url_path):
+    request = os.path.normpath((superstatic.config["WEB_ROOT"] + "/" + url_path))
+    entrypoint = None
+    if os.path.isfile(request):
+        entrypoint = request
+    elif os.path.isdir(request):
+        for key in reversed(EXTENSIONS):
+            test_entrypoint = request + "/index." + key
+            if os.path.isfile(test_entrypoint):
+                entrypoint = test_entrypoint
+    return entrypoint
 
 
 @superstatic.route("/")
 @superstatic.route("/<path:url_path>")
 def serve(url_path=""):
-    return 404
+    entrypoint = map_url(url_path)
+    return "OK"
 
 
 if __name__ == "__main__":
